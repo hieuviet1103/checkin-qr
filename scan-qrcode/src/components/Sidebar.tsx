e46a0df429e431371a85e359422e7d10e6dcdc2b
hieuviet1103 <hieuviet.1103@gmail.com>
2025-05-08 14:25:22 +0700

    update layout

--- scan-qrcode/src/components/Sidebar.tsx
+++ scan-qrcode/src/components/Sidebar.tsx
@@ -1,65 +1,78 @@
-'use client';
+"use client";
 
-import { useAuth } from '@/hooks/useAuth';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useAuth } from "@/hooks/useAuth";
+import Link from "next/link";
+import { redirect, usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Sidebar() {
   const pathname = usePathname();
   const [collapsed, setCollapsed] = useState(false);
   const { logout } = useAuth();
+  const { user } = useAuth();
 
   const toggleSidebar = () => {
     setCollapsed(!collapsed);
     // Lưu trạng thái vào localStorage để giữ nguyên trạng thái khi load lại trang
-    localStorage.setItem('sidebarCollapsed', String(!collapsed));
+    localStorage.setItem("sidebarCollapsed", String(!collapsed));
   };
 
   useEffect(() => {
     // Kiểm tra trạng thái sidebar từ localStorage
-    const savedState = localStorage.getItem('sidebarCollapsed');
-    if (savedState === 'true') {
+    const savedState = localStorage.getItem("sidebarCollapsed");
+    if (savedState === "true") {
       setCollapsed(true);
     }
   }, []);
 
+  const onLogout = () => {
+    logout();
+    redirect("/login");
+  };
+
   return (
-    <div 
-      className={`d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary ${collapsed ? 'sidebar-collapsed' : ''}`} 
-      style={{ 
-        width: collapsed ? '80px' : '280px',
-        transition: 'width 0.3s ease',
-        height: '100vh',
-        position: 'sticky',
+    <div
+      className={`d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary ${
+        collapsed ? "sidebar-collapsed" : ""
+      }`}
+      style={{
+        width: collapsed ? "80px" : "280px",
+        transition: "width 0.3s ease",
+        height: "100vh",
+        position: "sticky",
         top: 0,
-        overflowY: 'auto'
+        overflowY: "auto",
       }}
     >
       <div className="d-flex align-items-center justify-content-between">
         {!collapsed && (
-          <Link href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+          <Link
+            href="/"
+            className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+          >
             <svg className="bi pe-none me-2" width="40" height="32">
               <use xlinkHref="#pin-map-fill"></use>
             </svg>
             <span className="fs-4">Check-in</span>
           </Link>
         )}
-        <button 
-          className="btn btn-sm btn-outline-secondary" 
+        <button
+          className="btn btn-sm btn-outline-secondary"
           onClick={toggleSidebar}
           aria-label={collapsed ? "Mở rộng" : "Thu gọn"}
           title={collapsed ? "Mở rộng" : "Thu gọn"}
         >
-          {collapsed ? '→' : '←'}
+          {collapsed ? "→" : "←"}
         </button>
       </div>
       <hr />
       <ul className="nav nav-pills flex-column mb-auto">
         <li className="nav-item">
-          <Link 
-            href="/" 
-            className={`nav-link ${pathname === '/' ? 'active' : 'link-body-emphasis'}`}
+          <Link
+            href="/"
+            className={`nav-link ${
+              pathname === "/" ? "active" : "link-body-emphasis"
+            }`}
             data-title="Home"
           >
             <svg className="bi pe-none me-2" width="16" height="16">
@@ -69,9 +82,11 @@ export default function Sidebar() {
           </Link>
         </li>
         <li>
-          <Link 
-            href="/scan" 
-            className={`nav-link ${pathname === '/scan' ? 'active' : 'link-body-emphasis'}`}
+          <Link
+            href="/scan"
+            className={`nav-link ${
+              pathname === "/scan" ? "active" : "link-body-emphasis"
+            }`}
             data-title="Scan QR"
           >
             <svg className="bi pe-none me-2" width="16" height="16">
@@ -81,9 +96,11 @@ export default function Sidebar() {
           </Link>
         </li>
         <li>
-          <Link 
-            href="/checkin-result" 
-            className={`nav-link ${pathname === '/checkin-result' ? 'active' : 'link-body-emphasis'}`}
+          <Link
+            href="/checkin-result"
+            className={`nav-link ${
+              pathname === "/checkin-result" ? "active" : "link-body-emphasis"
+            }`}
             data-title="Checkin Result"
           >
             <svg className="bi pe-none me-2" width="16" height="16">
@@ -92,48 +109,63 @@ export default function Sidebar() {
             {!collapsed && <span>Check-in Result</span>}
           </Link>
         </li>
-        
+
+        {user && user.role === "admin" && (
+          <li>
+            <Link
+              href="/sessions"
+              className={`nav-link ${
+                pathname === "/sessions" ? "active" : "link-body-emphasis"
+              }`}
+              data-title="users"
+            >
+              <svg className="bi pe-none me-2" width="16" height="16">
+                <use xlinkHref="#people-circle"></use>
+              </svg>
+              {!collapsed && <span>Sessions</span>}
+            </Link>
+          </li>
+        )}
+        {user && user.role === "admin" && (
+          <li>
+            <Link
+              href="/groups"
+              className={`nav-link ${
+                pathname === "/groups" ? "active" : "link-body-emphasis"
+              }`}
+              data-title="users"
+            >
+              <svg className="bi pe-none me-2" width="16" height="16">
+                <use xlinkHref="#people-circle"></use>
+              </svg>
+              {!collapsed && <span>Groups</span>}
+            </Link>
+          </li>
+        )}
+        {user && user.role === "admin" && (
+          <li>
+            <Link
+              href="/users"
+              className={`nav-link ${
+                pathname === "/users" ? "active" : "link-body-emphasis"
+              }`}
+              data-title="users"
+            >
+              <svg className="bi pe-none me-2" width="16" height="16">
+                <use xlinkHref="#people-circle"></use>
+              </svg>
+              {!collapsed && <span>Users</span>}
+            </Link>
+          </li>
+        )}
+
         <li>
-          <Link 
-            href="/sessions" 
-            className={`nav-link ${pathname === '/sessions' ? 'active' : 'link-body-emphasis'}`}
-            data-title="users"
-          >
-            <svg className="bi pe-none me-2" width="16" height="16">
-              <use xlinkHref="#people-circle"></use>
-            </svg>
-            {!collapsed && <span>Sessions</span>}
-          </Link>
-        </li>
-        <li>
-          <Link 
-            href="/groups" 
-            className={`nav-link ${pathname === '/groups' ? 'active' : 'link-body-emphasis'}`}
-            data-title="users"
-          >
-            <svg className="bi pe-none me-2" width="16" height="16">
-              <use xlinkHref="#people-circle"></use>
-            </svg>
-            {!collapsed && <span>Groups</span>}
-          </Link>
-        </li>
-        <li>
-          <Link 
-            href="/users" 
-            className={`nav-link ${pathname === '/users' ? 'active' : 'link-body-emphasis'}`}
-            data-title="users"
-          >
-            <svg className="bi pe-none me-2" width="16" height="16">
-              <use xlinkHref="#people-circle"></use>
-            </svg>
-            {!collapsed && <span>Users</span>}
-          </Link>
-        </li>
-        <li>
-          <Link 
-            href="#" 
-            onClick={logout}
-            className={`nav-link ${pathname === '/login' ? 'active' : 'link-body-emphasis'}`}
+          <Link
+            href="#"
+            onClick={onLogout}
+            className={`nav-link ${
+              pathname === "/login" ? "active" : "link-body-emphasis"
+            }`}
             data-title="logout"
           >
             <svg className="bi pe-none me-2" width="16" height="16">
@@ -142,9 +174,8 @@ export default function Sidebar() {
             {!collapsed && <span>Logout</span>}
           </Link>
         </li>
-        
       </ul>
-      <hr />      
+      <hr />
     </div>
   );
-} 
+}
